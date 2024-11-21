d18804774b1d22e64f73b5638391a636ccc98073
Jeet solanki <solankijeet285@gmail.com>
2024-11-21 21:18:31 +0530

    JLSS added

--- src/App.js
+++ src/App.js
@@ -215,7 +215,9 @@ function App() {
       <h1>Focus, Bro!</h1>
       <p>If you can control your mind, don't look at girls and make a 3000 score.</p>
       <Game />
+      <a href='https://jlss.netlify.app/'>Powerd by JLSS</a>
     </div>
+    
   );
 }
 
